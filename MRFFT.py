b76823395ac169f3081955f4104cebc4a97837d2
Charles Milliaud <charles-edouard.milliaud@etudiant.univ-rennes1.fr>
2024-05-12 14:57:19 +0200

    MRFFT Round 1 and 2

--- MRFFT.py
+++ MRFFT.py
@@ -2,39 +2,49 @@ from pyspark import SparkContext, SparkConf
 import random as rand
 import math
 
-def MRFFT(P):
+def SequentialFFT(P,K):
     list = [element for element in P]
-    centers = [rand.choice(list)]  # Select a random point from inputPoints
+    centers = [rand.choice(list)] # we choose the first center randomly
     while len(centers) < K:
-        # we compute the farthest point from the existing centers
+        # we calculate the farthest point from the existing centers
         farthest_point = max(list, key=lambda point: min(math.dist(point, center) for center in centers))
         # we add the farthest point to the centers list
         centers.append(farthest_point)
-    print("Centers: ", centers, "List: ", list)
     return centers
+
+def MRFFT(P, K):
+    print("Starting MRFFT...")
+    partitions = P.repartition(10)
+    print("----------------- ROUND 1 -----------------\n")
+    centers_per_partition = partitions.mapPartitions(lambda partition: SequentialFFT(partition, K))
+    print("Centers per partition: ", centers_per_partition.collect(), "\n")
+    
+    print("----------------- ROUND 2 -----------------\n")
+    centers = SequentialFFT(centers_per_partition.collect(), K)
+    print("Centers: ", centers, "\n")
+
+    print("----------------- ROUND 3 -----------------\n")
+    print(type(P))
+    # R = max(P, key=lambda point: min(math.dist(point, center) for center in centers_per_partition.collect()))
+
+
+    # list = [element for element in partitions]
+    # centers = [rand.choice(list)]  # Select a random point from inputPoints
+    # while len(centers) < K:
+    #     # we compute the farthest point from the existing centers
+    #     farthest_point = max(list, key=lambda point: min(math.dist(point, center) for center in centers))
+    #     # we add the farthest point to the centers list
+    #     centers.append(farthest_point)
+    # print("Centers: ", centers, "List: ", list)
+    # return centers
 
 def main():
     print("Starting...")
     conf = SparkConf().setMaster("local").setAppName('MRFFT')
     sc = SparkContext(conf=conf)
-    global K 
-    K = 3    
-    # list = [[0.4, 0.9], [0.5, 4.1], [0.8, 0.91], [0.81, 1.1], [1.1, 5.0], [1.11, 5.1], [1.5, 1.1], [1.52, 1.11], [1.53,1.12], [1.54,1.13], 
-    #        [1.51,3.2], [1.52,3.6], [3.21,4.6], [4.11,4.11], [4.32,4.3]]
-    rawData = sc.textFile("artificial1M_9_100.csv").repartition(10)
+    rawData = sc.textFile("input.txt")
     rawData = rawData.map(lambda line: [float(i) for i in line.split(",")])
-    # Partition 1: ["0.4,0.9", "0.5,4.1", "0.8,0.91", "0.81,1.1"]
-    # Partition 2: ["1.1,5.0", "1.11,5.1", "1.5,1.1", "1.52,1.11"]
-    # inputPoints = rawData.map(lambda line: [float(i) for i in line.split(",")])
-    # print(rawData.glom().collect())
-    # Partition 1: [[0.4, 0.9], [0.5, 4.1], [0.8, 0.91], [0.81, 1.1]]
-    # Partition 2: [[1.1, 5.0], [1.11, 5.1], [1.5, 1.1], [1.52, 1.11]]
-    # P = inputPoints.collect() to collect all points in a list
-    centers = rawData.mapPartitions(MRFFT).collect()
-    RDDcenters = sc.parallelize(centers)
-    centers = RDDcenters.mapPartitions(MRFFT).collect()
-    print("Final centers of the first round:", centers,"\n")
-    # centers = centers.reduce(MRFFT)
+    MRFFT(rawData, 3)
 
 if __name__ == "__main__":
     main()
