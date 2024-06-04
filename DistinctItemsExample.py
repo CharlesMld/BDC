f5279127a633d67bbd7fe95c7ec5e080806ec7ab
Charles Milliaud <charles-edouard.milliaud@etudiant.univ-rennes1.fr>
2024-06-04 11:03:45 +0200

    main up to date

---
+++ DistinctItemsExample.py
@@ -0,0 +1,114 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark import StorageLevel
+import threading
+import sys
+
+# After how many items should we stop?
+THRESHOLD = -1 # To be set via command line
+
+
+# Operations to perform after receiving an RDD 'batch' at time 'time'
+def process_batch(time, batch):
+    # We are working on the batch at time `time`.
+    global streamLength, histogram
+    batch_size = batch.count()
+    # If we already have enough points (> THRESHOLD), skip this batch.
+    if streamLength[0]>=THRESHOLD:
+        return
+    streamLength[0] += batch_size
+    # Extract the distinct items from the batch
+    batch_items = batch.map(lambda s: (int(s), 1)).reduceByKey(lambda i1, i2: 1).collectAsMap()
+
+    # Update the streaming state
+    for key in batch_items:
+        if key not in histogram:
+            histogram[key] = 1
+            
+    # If we wanted, here we could run some additional code on the global histogram
+    if batch_size > 0:
+        print("Batch size at time [{0}] is: {1}".format(time, batch_size))
+
+    if streamLength[0] >= THRESHOLD:
+        stopping_condition.set()
+        
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 3, "USAGE: port, threshold"
+
+    # IMPORTANT: when running locally, it is *fundamental* that the
+    # `master` setting is "local[*]" or "local[n]" with n > 1, otherwise
+    # there will be no processor running the streaming computation and your
+    # code will crash with an out of memory (because the input keeps accumulating).
+    conf = SparkConf().setMaster("local[*]").setAppName("DistinctExample")
+    # If you get an OutOfMemory error in the heap consider to increase the
+    # executor and drivers heap space with the following lines:
+    # conf = conf.set("spark.executor.memory", "4g").set("spark.driver.memory", "4g")
+    
+    
+    # Here, with the duration you can control how large to make your batches.
+    # Beware that the data generator we are using is very fast, so the suggestion
+    # is to use batches of less than a second, otherwise you might exhaust the memory.
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, 0.01)  # Batch duration of 0.01 seconds
+    ssc.sparkContext.setLogLevel("ERROR")
+    
+    # TECHNICAL DETAIL:
+    # The streaming spark context and our code and the tasks that are spawned all
+    # work concurrently. To ensure a clean shut down we use this semaphore.
+    # The main thread will first acquire the only permit available and then try
+    # to acquire another one right after spinning up the streaming computation.
+    # The second tentative at acquiring the semaphore will make the main thread
+    # wait on the call. Then, in the `foreachRDD` call, when the stopping condition
+    # is met we release the semaphore, basically giving "green light" to the main
+    # thread to shut down the computation.
+    # We cannot call `ssc.stop()` directly in `foreachRDD` because it might lead
+    # to deadlocks.
+    stopping_condition = threading.Event()
+    
+    
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    # INPUT READING
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    portExp = int(sys.argv[1])
+    print("Receiving data from port =", portExp)
+    
+    THRESHOLD = int(sys.argv[2])
+    print("Threshold = ", THRESHOLD)
+        
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    # DEFINING THE REQUIRED DATA STRUCTURES TO MAINTAIN THE STATE OF THE STREAM
+    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    streamLength = [0] # Stream length (an array to be passed by reference)
+    histogram = {} # Hash Table for the distinct elements
+    
+
+    # CODE TO PROCESS AN UNBOUNDED STREAM OF DATA IN BATCHES
+    stream = ssc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevel.MEMORY_AND_DISK)
+    # For each batch, to the following.
+    # BEWARE: the `foreachRDD` method has "at least once semantics", meaning
+    # that the same data might be processed multiple times in case of failure.
+    stream.foreachRDD(lambda time, batch: process_batch(time, batch))
+    
+    # MANAGING STREAMING SPARK CONTEXT
+    print("Starting streaming engine")
+    ssc.start()
+    print("Waiting for shutdown condition")
+    stopping_condition.wait()
+    print("Stopping the streaming engine")
+    # NOTE: You will see some data being processed even after the
+    # shutdown command has been issued: This is because we are asking
+    # to stop "gracefully", meaning that any outstanding work
+    # will be done.
+    ssc.stop(False, True)
+    print("Streaming engine stopped")
+
+    # COMPUTE AND PRINT FINAL STATISTICS
+    print("Number of items processed =", streamLength[0])
+    print("Number of distinct items =", len(histogram))
+    largest_item = max(histogram.keys())
+    print("Largest item =", largest_item)
+    
---
+++ G064HW2_final.py
@@ -0,0 +1,193 @@
+from pyspark import SparkContext, SparkConf
+import sys
+import os
+import random as rand
+import math
+import time
+
+
+"""
+MRApproxOutliers
+"""
+def MRApproxOutliers(inputPoints, D, M): 
+    start_time = time.time()
+    
+    omega = D/(2*math.sqrt(2))
+    
+    # Given an RDD of points returns the cell each point(element of the RDD) belongs to
+    def pointToCell(iterator):
+        result = []
+
+        for point in iterator:
+            cell_x = int(math.floor(point[0] / omega))
+            cell_y = int(math.floor(point[1] / omega))
+            cell = (cell_x, cell_y)
+            result.append((cell,1))
+
+        return result
+    
+    # Step A
+    # cells_counts is an RDD whose elements are (cell, number of points)
+    cells_counts = inputPoints.mapPartitions(pointToCell).reduceByKey(lambda a,b: a + b)
+    # Assuming there are few non-empty cells I can save the RDD in a dictionary
+    cells_counts_dict = cells_counts.collectAsMap()
+    
+    
+
+    # Step B
+    # These 2 functions count for each cell the number of points in a 7x7 and 3x3 region of cells respectively
+    def region_counts7(cell_counts):
+        cell, _ = cell_counts
+        x, y = cell
+        total_count = 0
+        for i in range(x - 3, x + 4):
+            for j in range(y - 3, y + 4):
+                if (i, j) in cells_counts_dict:
+                    total_count += cells_counts_dict[(i, j)]
+
+        return (cell, total_count)
+    
+    def region_counts3(cell_counts):
+        cell, _ = cell_counts
+        x, y = cell
+        total_count = 0
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if (i, j) in cells_counts_dict:
+                    total_count += cells_counts_dict[(i, j)]
+
+        return (cell, total_count)
+
+
+    
+    # Filter operation to select the cells sure and uncertain outlier cells 
+    outlierCells = cells_counts.map(region_counts7).filter(lambda x: x[1] <= M).collectAsMap()
+    uncertainCells = cells_counts.map(region_counts3).filter(lambda x: x[1] <= M and x[0] not in outlierCells).collectAsMap()
+
+    # Count sure outliers and uncertain points
+    outlierPoints = inputPoints.filter(lambda x: (int(math.floor(x[0] / omega)), int(math.floor(x[1] / omega))) in outlierCells).count()
+    uncertainPoints = inputPoints.filter(lambda x: (int(math.floor(x[0] / omega)), int(math.floor(x[1] / omega))) in uncertainCells).count()
+
+    print("Number of sure outliers =", outlierPoints,"\nNumber of uncertain points =",uncertainPoints )
+
+    # Running time
+    end_time = time.time()
+    running_time_ms = int((end_time - start_time) * 1000)
+    print("Running time of MRApproxOutliers =", running_time_ms, "ms")
+    return outlierPoints
+
+
+"""
+MRFFT
+"""
+def find_duplicate_points(point_list):
+    # Create an empty dictionary to store the counts of each point
+    point_list = [tuple(point) for point in point_list]
+    point_counts = {}
+
+    # Iterate through the list of points and count occurrences
+    for point in point_list:
+        if point in point_counts:
+            point_counts[point] += 1
+        else:
+            point_counts[point] = 1
+
+    # Filter points that have count greater than 1 (i.e., duplicates)
+    duplicate_points = [(point,count) for point, count in point_counts.items() if count > 1]
+
+    return duplicate_points
+
+def SequentialFFT(P, K):
+    rand.seed(42)
+    centers = [rand.choice(P)]
+    distances = [[i, math.dist(point, centers[0])] for i, point in enumerate(P)]
+
+    while len(centers) < K:
+        # Find the farthest point
+        farthest_point_index, _ = max(distances, key=lambda x: x[1])
+        farthest_point = P[farthest_point_index]
+        
+        # Update distances for the newly added center
+        for i, point in enumerate(P):
+            if point not in centers:
+                distances[i][1] = min(distances[i][1], math.dist(point, farthest_point))
+            else:
+                pass
+
+        centers.append(farthest_point)
+            
+    return centers
+
+
+
+def MRFFT(P, K):
+    # ROUND 1
+    st = time.time()
+    
+    centers_per_partition = P.mapPartitions(lambda partition: SequentialFFT(list(partition),K))
+    centers_count = centers_per_partition.count()
+    centers_per_partition.cache()
+    
+    et = time.time()
+    print(f"Running time of MRFFT Round 1 = {int((et - st) * 1000)} ms")
+    
+    # ROUND 2
+    st = time.time()
+    
+    aggregated_centers = centers_per_partition.collect()
+    C = SequentialFFT(aggregated_centers, K)
+    print(f"centers={C}") # run SequentialFFT again to get the final set of centers
+    
+    et = time.time()
+    print(f"Running time of MRFFT Round 2 = {int((et - st) * 1000)} ms")
+
+    # ROUND 3
+    st = time.time()
+    
+    context = SparkContext.getOrCreate()
+    broadcast_C = context.broadcast(C)
+    FarthestPoint = P.map(lambda point: min(math.dist(point, center) for center in broadcast_C.value)).reduce(max)
+    
+    et = time.time()
+    print(f"Running time of MRFFT Round 3 = {int((et - st) * 1000)} ms")
+    
+    print(f"Radius = {round(FarthestPoint,8)}")
+    return FarthestPoint
+
+
+"""
+main
+"""
+def main():
+    # spark setup
+    conf = SparkConf().setAppName('G064HW2')
+    conf.set("spark.locality.wait", "0s");
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN")
+
+    # check and process command line args
+    assert len(sys.argv) == 5, "Usage: python G064HW2.py <file_name> <M> <K> <L>"
+    
+    data_path = sys.argv[1]
+    M = int(sys.argv[2])
+    K = int(sys.argv[3])
+    L = int(sys.argv[4])
+
+    # print command line args
+    print(f"{data_path} M={M} K={K} L={L}")
+    
+    # read data
+    rawData = sc.textFile(data_path).repartition(numPartitions=L)
+    inputPoints = rawData.map(lambda line: [float(i) for i in line.split(",")])
+    
+    #inputPoints.cache()
+    #input = inputPoints.collect()
+    print(f"Number of points = {inputPoints.count()}")
+
+    D = MRFFT(inputPoints, K)
+    MRApproxOutliers(inputPoints, D, M)
+    #dup = find_duplicate_points(input)
+    #print(f"dup={len(dup)}")
+
+if __name__ == "__main__":
+	main()
